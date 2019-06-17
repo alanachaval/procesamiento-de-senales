4c0d858ed161130fab5db053a3246cab5f09060f
Alan Achaval <achaval.alan.n@gmail.com>
2019-06-17 19:27:44 -0300

    Rename plot a plt

--- Practica5/Ejercicio14.py
+++ Practica5/Ejercicio14.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import numpy as np
 
 from Practica5.Funciones import Funciones
@@ -6,35 +6,35 @@ from Practica5.Funciones import Funciones
 tiempo = np.linspace(0, 1, 5000)
 senal = Funciones.generar_diente_de_sierra(tiempo, 3, 20, np.pi)
 
-f1 = plot.figure(1)
+f1 = plt.figure(1)
 f1.canvas.set_window_title('Diente de sierra periodica')
-plot.xlabel('Tiempo (en segundos)')
-plot.ylabel('Amplitud')
-plot.plot(tiempo, senal)
+plt.xlabel('Tiempo (en segundos)')
+plt.ylabel('Amplitud')
+plt.plot(tiempo, senal)
 
 senal_fft = Funciones.transformada_de_fourier(senal)
-f2 = plot.figure(2)
+f2 = plt.figure(2)
 f2.canvas.set_window_title('Transformada diente de sierra periodica')
-plot.xlabel('Frecuencia (en hertz)')
-plot.ylabel('Amplitud')
+plt.xlabel('Frecuencia (en hertz)')
+plt.ylabel('Amplitud')
 senal_fft_plot, frecuencia = Funciones.linspace_transformada(senal_fft, tiempo)
-plot.plot(frecuencia, senal_fft)
+plt.plot(frecuencia, senal_fft)
 
 senal_no_periodica = np.zeros(5000)
 for i in range(250 * 9, 250 * 10):
     senal_no_periodica[i] = senal[i]
-f3 = plot.figure(3)
+f3 = plt.figure(3)
 f3.canvas.set_window_title('Diente de sierra')
-plot.xlabel('Tiempo (en segundos)')
-plot.ylabel('Amplitud')
-plot.plot(tiempo, senal_no_periodica)
+plt.xlabel('Tiempo (en segundos)')
+plt.ylabel('Amplitud')
+plt.plot(tiempo, senal_no_periodica)
 
 senal_no_periodica_fft = Funciones.transformada_de_fourier(senal_no_periodica)
-f4 = plot.figure(4)
+f4 = plt.figure(4)
 f4.canvas.set_window_title('Transformada diente de sierra')
-plot.xlabel('Frecuencia (en hertz)')
-plot.ylabel('Amplitud')
+plt.xlabel('Frecuencia (en hertz)')
+plt.ylabel('Amplitud')
 senal_no_periodica_fft_plot, frecuencia = Funciones.linspace_transformada(senal_no_periodica_fft, tiempo)
-plot.plot(frecuencia, senal_no_periodica_fft_plot)
+plt.plot(frecuencia, senal_no_periodica_fft_plot)
 
-plot.show()
+plt.show()
--- Practica5/Ejercicio2.py
+++ Practica5/Ejercicio2.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fftpack import ifft
 
@@ -7,32 +7,32 @@ from Practica5.Funciones import Funciones
 tiempo = np.linspace(0, 1, 5000)
 senal = Funciones.generar_senoidal(tiempo, 3, 20, 0)
 
-f1 = plot.figure(1)
+f1 = plt.figure(1)
 f1.canvas.set_window_title('Original: Sin')
-plot.xlabel('Tiempo (en segundos)')
-plot.ylabel('Amplitud')
-plot.plot(tiempo, senal)
+plt.xlabel('Tiempo (en segundos)')
+plt.ylabel('Amplitud')
+plt.plot(tiempo, senal)
 
-f2 = plot.figure(2)
+f2 = plt.figure(2)
 f2.canvas.set_window_title('Transformada: F(Sin)')
-plot.xlabel('Frecuencia (en hertz)')
-plot.ylabel('Amplitud')
+plt.xlabel('Frecuencia (en hertz)')
+plt.ylabel('Amplitud')
 senal_fft = Funciones.transformada_de_fourier(senal)
 senal_fft_plot, frecuencia = Funciones.linspace_transformada(senal_fft, tiempo)
-plot.plot(frecuencia, senal_fft_plot)
+plt.plot(frecuencia, senal_fft_plot)
 
-f3 = plot.figure(3)
+f3 = plt.figure(3)
 f3.canvas.set_window_title('Anti-Transformada: F-1(F(Sin))')
-plot.xlabel('Tiempo (en segundos)')
-plot.ylabel('Amplitud')
+plt.xlabel('Tiempo (en segundos)')
+plt.ylabel('Amplitud')
 senal_ifft = Funciones.antitrasformada_de_fourier(senal_fft)
-plot.plot(tiempo, senal_ifft)
+plt.plot(tiempo, senal_ifft)
 
-f4 = plot.figure(4)
+f4 = plt.figure(4)
 f4.canvas.set_window_title('Anti-Transformada: F-1(F(Sin)). Mediante scipy, como referencia')
-plot.xlabel('Tiempo (en segundos)')
-plot.ylabel('Amplitud')
+plt.xlabel('Tiempo (en segundos)')
+plt.ylabel('Amplitud')
 senal_ifft2 = ifft(senal_fft)
-plot.plot(tiempo, senal_ifft2)
+plt.plot(tiempo, senal_ifft2)
 
-plot.show()
+plt.show()
