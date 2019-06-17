49c228bf276a4cf6993bad6b5aa0b2b39fe21f0a
Alan Achaval <achaval.alan.n@gmail.com>
2019-06-17 20:16:47 -0300

    Separacion codigo de logica y ploteo

--- Practica5/Ejercicio14.py
+++ Practica5/Ejercicio14.py
@@ -5,6 +5,13 @@ from Practica5.Funciones import Funciones
 
 tiempo = np.linspace(0, 1, 5000)
 senal = Funciones.generar_diente_de_sierra(tiempo, 3, 20, np.pi)
+senal_fft = Funciones.transformada_de_fourier(senal)
+senal_fft_plot, frecuencia = Funciones.linspace_transformada(senal_fft, tiempo)
+senal_no_periodica = np.zeros(5000)
+for i in range(250 * 9, 250 * 10):
+    senal_no_periodica[i] = senal[i]
+senal_no_periodica_fft = Funciones.transformada_de_fourier(senal_no_periodica)
+senal_no_periodica_fft_plot, frecuencia = Funciones.linspace_transformada(senal_no_periodica_fft, tiempo)
 
 f1 = plt.figure(1)
 f1.canvas.set_window_title('Diente de sierra periodica')
@@ -12,29 +19,22 @@ plt.xlabel('Tiempo (en segundos)')
 plt.ylabel('Amplitud')
 plt.plot(tiempo, senal)
 
-senal_fft = Funciones.transformada_de_fourier(senal)
 f2 = plt.figure(2)
 f2.canvas.set_window_title('Transformada diente de sierra periodica')
 plt.xlabel('Frecuencia (en hertz)')
 plt.ylabel('Amplitud')
-senal_fft_plot, frecuencia = Funciones.linspace_transformada(senal_fft, tiempo)
 plt.plot(frecuencia, senal_fft)
 
-senal_no_periodica = np.zeros(5000)
-for i in range(250 * 9, 250 * 10):
-    senal_no_periodica[i] = senal[i]
 f3 = plt.figure(3)
 f3.canvas.set_window_title('Diente de sierra')
 plt.xlabel('Tiempo (en segundos)')
 plt.ylabel('Amplitud')
 plt.plot(tiempo, senal_no_periodica)
 
-senal_no_periodica_fft = Funciones.transformada_de_fourier(senal_no_periodica)
 f4 = plt.figure(4)
 f4.canvas.set_window_title('Transformada diente de sierra')
 plt.xlabel('Frecuencia (en hertz)')
 plt.ylabel('Amplitud')
-senal_no_periodica_fft_plot, frecuencia = Funciones.linspace_transformada(senal_no_periodica_fft, tiempo)
 plt.plot(frecuencia, senal_no_periodica_fft_plot)
 
 plt.show()
--- Practica5/Ejercicio2.py
+++ Practica5/Ejercicio2.py
@@ -6,6 +6,10 @@ from Practica5.Funciones import Funciones
 
 tiempo = np.linspace(0, 1, 5000)
 senal = Funciones.generar_senoidal(tiempo, 3, 20, 0)
+senal_fft = Funciones.transformada_de_fourier(senal)
+senal_fft_plot, frecuencia = Funciones.linspace_transformada(senal_fft, tiempo)
+senal_ifft = Funciones.antitrasformada_de_fourier(senal_fft)
+senal_ifft2 = ifft(senal_fft)
 
 f1 = plt.figure(1)
 f1.canvas.set_window_title('Original: Sin')
@@ -17,22 +21,18 @@ f2 = plt.figure(2)
 f2.canvas.set_window_title('Transformada: F(Sin)')
 plt.xlabel('Frecuencia (en hertz)')
 plt.ylabel('Amplitud')
-senal_fft = Funciones.transformada_de_fourier(senal)
-senal_fft_plot, frecuencia = Funciones.linspace_transformada(senal_fft, tiempo)
 plt.plot(frecuencia, senal_fft_plot)
 
 f3 = plt.figure(3)
 f3.canvas.set_window_title('Anti-Transformada: F-1(F(Sin))')
 plt.xlabel('Tiempo (en segundos)')
 plt.ylabel('Amplitud')
-senal_ifft = Funciones.antitrasformada_de_fourier(senal_fft)
 plt.plot(tiempo, senal_ifft)
 
 f4 = plt.figure(4)
 f4.canvas.set_window_title('Anti-Transformada: F-1(F(Sin)). Mediante scipy, como referencia')
 plt.xlabel('Tiempo (en segundos)')
 plt.ylabel('Amplitud')
-senal_ifft2 = ifft(senal_fft)
 plt.plot(tiempo, senal_ifft2)
 
 plt.show()
